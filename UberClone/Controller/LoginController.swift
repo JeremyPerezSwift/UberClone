667a7a1007cef130f90b95f4b151e85fca6a9d23
JeremyPerezSwift <jeremyperez34000@gmail.com>
2020-10-06 18:07:31 +0200

    Getting started with login UI

--- UberClone/Controller/LoginController.swift
+++ UberClone/Controller/LoginController.swift
@@ -19,6 +19,37 @@ class LoginController: UIViewController {
         return label
     }()
     
+    private lazy var emailContaineView: UIView = {
+        let view = UIView()
+        
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "mail")
+        imageView.alpha = 0.8
+        view.addSubview(imageView)
+        imageView.centerY(inView: view)
+        imageView.anchor(left: view.leftAnchor, paddingLeft: 8, width: 24, height: 24)
+        
+        view.addSubview(emailTextField)
+        emailTextField.anchor(top: view.topAnchor, left: imageView.rightAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 8, paddingLeft: 8, paddingBottom: 8, paddingRight: 8)
+        
+        let separatorView = UIView()
+        separatorView.backgroundColor = .lightGray
+        view.addSubview(separatorView)
+        separatorView.anchor(left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor,paddingLeft: 8, height: 0.75)
+        
+        return view
+    }()
+    
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.borderStyle = .none
+        textField.font = UIFont.systemFont(ofSize: 16)
+        textField.textColor = .white
+        textField.keyboardAppearance = .dark
+        textField.attributedPlaceholder = NSAttributedString(string: "Email", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray])
+        return textField
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -29,6 +60,9 @@ class LoginController: UIViewController {
         view.addSubview(titleLabel)
         titleLabel.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 8)
         titleLabel.centerX(inView: view)
+        
+        view.addSubview(emailContaineView)
+        emailContaineView.anchor(top: titleLabel.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 40, paddingLeft: 16, paddingRight: 16, height: 50)
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
